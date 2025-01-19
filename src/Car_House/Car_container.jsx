eb00ba8c8d241c6c7526b699b312d561e161ddc7
Rakibul_Hassan <mdrakibulhassanshehab@gmail.com>
2025-01-19 08:28:13 +0600

    Cars House Search and isPrimeum functionlites added

--- src/Car_House/Car_container.jsx
+++ src/Car_House/Car_container.jsx
@@ -1,16 +1,25 @@
 
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Search from './Search'
 import Car_Lists from './Car_Lists'
+const cars = [   
+  { id: 1, title: "Luxury Sedan", brand: "Mercedes", year: 2023, price: 80000, isPremium: true },
+  { id: 2, title: "Family SUV", brand: "Toyota", year: 2022, price: 45000, isPremium: false },
+  { id: 3, title: "Sports Car", brand: "Porsche", year: 2023, price: 120000, isPremium: true },
+  { id: 4, title: "Electric Hatchback", brand: "Nissan", year: 2022, price: 35000, isPremium: false },
+  { id: 5, title: "Luxury SUV", brand: "BMW", year: 2023, price: 90000, isPremium: true },
+]
 
 const Car_container = () => {
+  const [searchTerms,setSearchTerms] = useState('');
+  const [isPremium,setIsPremium] = useState(false);
   return (
     <div className='p-5 m-auto'>
       <Header text={"Car House"}/>
-      <Search />
-      <Car_Lists />
+      <Search searchTerms={searchTerms} setSearchTerms={setSearchTerms} isPremium={isPremium} setIsPremium={setIsPremium}/>
+      <Car_Lists cars={cars} searchTerms={searchTerms} isPremium={isPremium} />
     </div>
   )
 }
--- src/Car_House/Search.jsx
+++ src/Car_House/Search.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({searchTerms,setSearchTerms,isPremium ,setIsPremium}) => {
   return (
     <div className='w-full flex p-5 items-center'>
-      <input className='lg:w-[400px] p-2 shadow-md mx-5 border-2 border-black' type="text" placeholder='Search Here...' />
+      <input value={searchTerms} onChange={(e) => setSearchTerms(e.target.value)} className='lg:w-[400px] p-2 shadow-md mx-5 border-2 border-black' type="text" placeholder='Search Here...' />
       <div>
-        <input className='' type="checkbox" name="primeum" id="" />
+        <input className='' onChange={(e) =>setIsPremium(e.target.checked)} type="checkbox" name="primeum" id="" />
         <span>Show Priemium Only</span>
       </div>
     </div>
